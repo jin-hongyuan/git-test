ff2b86a81a14e3ee2ec767919b974ffbb2ad3cf9
jin_hy <jin_hy@murongtech.com>
2024-08-20 20:46:38 +0800

    try

--- src/main/java/com/yuanhao/App.java
+++ src/main/java/com/yuanhao/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println("Hello,World2");
         System.out.println("Hello,World4");
         System.out.println("develop");
+        System.out.println("try");
     }
 }
